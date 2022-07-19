0dfa70ddb65dd5e2907c3b287e1a3d9b34c1cd9a
msabelhaus <70977660+msabelhaus@users.noreply.github.com>
2022-07-18 22:03:08 -0600

    Adding completed train/inference functions, plus config and requirements files

---
+++ src/config.py
@@ -0,0 +1,2 @@
+# data
+MODEL_DIR = "../models/"
--- src/train.py
+++ src/train.py
@@ -1,14 +1,20 @@
 # Modules
+import os
+import pickle
+
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+import config
+from MNB_model import my_MNB
 
 def run_training():
     """
     Training ML model 
     """
 
+    # Load in data
     news = fetch_20newsgroups(subset="all")
     
     # Extract the messages and topic labels, and view the topic labels
@@ -22,11 +28,22 @@ def run_training():
     tfidfer = TfidfVectorizer()
     tfidfer.fit(X_train)
     X_train_tfidf = tfidfer.transform(X_train)
-    X_test_tfidf = tfidfer.transform(X_test)
     
-    # Load the classifier
+    # Path to save model
+    model_path = f"{config.MODEL_DIR}/"
+
+    # Checking the folder exist
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
     
+    # Train a single model
+    mnb = my_MNB()
+    mnb.fit(X_train_tfidf, y_train)
+    
+    # save the model to disk
+    filename = 'MNB_trained.sav'
+    pickle.dump(mnb, open(f"{model_path}/{filename}", 'wb'))
 
 
 if __name__ == "__main__":
-    run_training("LSTM")
+    run_training()
